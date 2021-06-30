65768bb10e98df7014bea2b10c77ff3ad8da3477
Muxammed Safarov <69106365+Maga077@users.noreply.github.com>
2021-06-30 14:55:55 +0400

    Create 8953-Print array.cpp

---
+++ 8953-Print array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int n;
+	cin>>n;
+	int a[n];
+	int i;
+	for(i==0;i<n;i++){
+
+	cin>>a[i];
+	cout<<a[i]<<endl;}
+	
+	return 0;
+}
